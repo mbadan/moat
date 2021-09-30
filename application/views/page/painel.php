e4de3cbb54894f9c5b70b380033832cd51c755ca
Mateus Badan de Pieri <mateuspieri@gmail.com>
2021-09-30 02:07:21 -0300

    add bootstrap + prototype painel page

--- application/views/page/painel.php
+++ application/views/page/painel.php
@@ -1,15 +1,86 @@
 <!DOCTYPE html>
 <html class="">
 <?php $this->load->view('component/head'); ?>
+<link rel="stylesheet" href="/public/css/bootstrap.min.css" />
 </head>
-<body class="align">
+  <body>
 
-  <div class="grid">
+    <header>
+    </header>
 
+    <main role="main">
 
-  </div>
+      <section class="jumbotron text-center banner-session">
+        <div class="container">
+          <h1 class="jumbotron-heading">Music App Example</h1>
+          <p class="lead text-muted">Here you can add, delete or just edit any album that you like</p>
+          <p>
+            <button type="button" class="btn btn-secondary my-2" data-toggle="modal" data-target="#exampleModal">Add Album</button>
+          </p>
+        </div>
+      </section>
 
-</body>
+      <div class="album py-5 bg-light">
+        <div class="container">
+          <div class="row">
+            <div class="col-md-4">
+              <div class="card mb-4 box-shadow">
+                <img class="card-img-top" src="/public/images/vinyl.png" alt="Card image cap">
+                <div class="card-body">
+                  <p class="card-text">This is a wider card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
+                  <div class="d-flex justify-content-between align-items-center">
+                    <div class="btn-group">
+                      <button type="button" class="btn btn-sm btn-outline-secondary">Edit</button>
+                      <button type="button" class="btn btn-sm btn-outline-secondary">Delete</button>
+                    </div>
+                    <small class="text-muted">Usuário</small>
+                  </div>
+                </div>
+              </div>
+            </div>
+            <div class="col-md-4">
+              <div class="card mb-4 box-shadow">
+                <img class="card-img-top" src="/public/images/vinyl.png" alt="Card image cap">
+                <div class="card-body">
+                  <p class="card-text">This is a wider card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
+                  <div class="d-flex justify-content-between align-items-center">
+                    <div class="btn-group">
+                      <button type="button" class="btn btn-sm btn-outline-secondary">View</button>
+                      <button type="button" class="btn btn-sm btn-outline-secondary">Edit</button>
+                    </div>
+                    <small class="text-muted">Usuário</small>
+                  </div>
+                </div>
+              </div>
+            </div>
+            <div class="col-md-4">
+              <div class="card mb-4 box-shadow">
+                <img class="card-img-top" src="/public/images/vinyl.png" alt="Card image cap">
+                <div class="card-body">
+                  <p class="card-text">This is a wider card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
+                  <div class="d-flex justify-content-between align-items-center">
+                    <div class="btn-group">
+                      <button type="button" class="btn btn-sm btn-outline-secondary">View</button>
+                      <button type="button" class="btn btn-sm btn-outline-secondary">Edit</button>
+                    </div>
+                    <small class="text-muted">Usuário</small>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+
+    </main>
+
+    <!-- Bootstrap core JavaScript
+    ================================================== -->
+    <!-- Placed at the end of the document so the pages load faster -->
+    <script src="https://code.jquery.com/jquery-3.2.1.slim.min.js" integrity="sha384-KJ3o2DKtIkvYIK3UENzmM7KCkRr/rE9/Qpg6aAZGJwFDMVNA/GpGFF93hXpG5KkN" crossorigin="anonymous"></script>
+    <script>window.jQuery || document.write('<script src="../../../../assets/js/vendor/jquery-slim.min.js"><\/script>')</script>
+    <script src="../../../../public/js/bootstrap.min.js"></script>
+  </body>
 <?php $this->load->view('component/js'); ?>
 <script type="text/javascript">
   
